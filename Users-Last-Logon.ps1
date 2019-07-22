2f5c4179d0895de6d42cc7b5461daf2e09f7f116
David Luttig <dluttig@toddcorporation.com>
2019-07-22 15:24:32 +1200

    commit

---
+++ Users-Last-Logon.ps1
@@ -0,0 +1,21 @@
+﻿$NumDays = 0
+ $LogDir = "C:\temp\Users-Last-Logon.csv"
+
+$currentDate = [System.DateTime]::Now
+ $currentDateUtc = $currentDate.ToUniversalTime()
+ $lltstamplimit = $currentDateUtc.AddDays(- $NumDays)
+ $lltIntLimit = $lltstampLimit.ToFileTime()
+ $adobjroot = [adsi]''
+ $objstalesearcher = New-Object System.DirectoryServices.DirectorySearcher($adobjroot)
+ $objstalesearcher.filter = "(&(objectCategory=person)(objectClass=user)(lastLogonTimeStamp<=" + $lltIntLimit + "))"
+ $objstalesearcher.PageSize=4000
+
+$users =  $objstalesearcher.findall() | select `
+ @{e={$_.properties.cn};n='Display Name'},`
+ @{e={$_.properties.samaccountname};n='Username'},`
+ @{e={$_.properties.mail};n='Email'},`
+ @{e={[datetime]::FromFileTimeUtc([int64]$_.properties.lastlogontimestamp[0])};n='Last Logon'},`
+ @{e={[datetime]::FromFileTimeUtc([int64]$_.properties.pwdlastset[0])};n='Password Last set'},`
+ @{e={[string]$adspath=$_.properties.adspath;$account=[ADSI]$adspath;$account.psbase.invokeget('AccountDisabled')};n='Account Is Disabled'}
+
+$users  | Export-CSV -NoType $LogDir
